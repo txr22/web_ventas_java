0f1d2d00556cad8ece55732ed28e6d3e50d103a1
chema <chema22web@gmail.com>
2019-06-05 11:03:22 +0200

    prueba formulario, y 2 componentes

--- angular_webVentas/webVentas/src/app/app.module.ts
+++ angular_webVentas/webVentas/src/app/app.module.ts
@@ -4,14 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ListaComponent } from './componentes/lista/lista.component';
 import { FormularioComponent } from './componentes/formulario/formulario.component';
-import { ServicioWebComponent } from './servicio-web/servicio-web.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaComponent,
     FormularioComponent,
-    ServicioWebComponent
+   
   ],
   imports: [
     BrowserModule
--- angular_webVentas/webVentas/src/app/componentes/formulario/formulario.component.ts
+++ angular_webVentas/webVentas/src/app/componentes/formulario/formulario.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
+  prueba = 'prueba de formulario';
   constructor() { }
 
   ngOnInit() {
